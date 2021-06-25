c3828e8234b0fdfa2878ff5c0730273f10353c24
MikeAlpine <anthonylou348@gmail.com>
2021-06-25 13:03:32 -0500

    replaced resource.lua with fxmanifest.lua

---
+++ Solo-Indicators/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'adamant'
+
+author 'OfficerSolo'
+description 'Custom Indicator Script created by OfficerSolo'
+
+------------------------------
+-- Made By 0fficerSolo#0001 --
+------------------------------
+
+server_script 'sv_indicator.lua'
+client_script 'cl_indicator.lua'
